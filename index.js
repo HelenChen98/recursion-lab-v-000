343972439ef885156eae5d80816cc9438eb555c7
Karen C <chenyanhuan66@163.com>
2018-08-24 08:28:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
-// Code your solution here!
+function printString(myString) {
+  console.log(myString[0]);
+
+  if (myString.length > 1) {
+    let mySubString = myString.substring(1, myString.length);
+    printString(mySubString);
+  } else {
+    return true;
+  }
+}
+
+function reverseString(mystring){
+  if (mystring === ""){ // This is the terminal case that will end the recursion
+    return "";
+  }
+  else{
+    return reverseString(mystring.substr(1)) + mystring.charAt(0);
+  }
+
+}
+
+function isPalindrome(mystring){
+
+  return mystring == reverseString(mystring);
+}
+
 
